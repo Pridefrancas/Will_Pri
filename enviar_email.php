63faba7ce99ff2dcef068fed7e1e68d77b737f33
willianfrancas <willianfrancas@gmail.com>
2019-01-15 18:20:00 -0200

    News features
    Redimensionado o tamanho das imagens para um carregamento mais leve aos clientes/visitantes.
    
    Adicionado a buques.html um formulario para envio de email a partir do site.
    Adicionado formail.css para estilo .css do formulario
    Adicionado pagina .php enviar_email.php
    
    A pagina casamentos.html foi alterada para contato.html que sera a confirmação ou não do envio do email (ainda trabalhando nisso).

---
+++ enviar_email.php
@@ -0,0 +1,29 @@
+<?
+# alterar a variavel abaixo colocando o seu email
+
+//$destinatario = "contato@deflorealma.com.br";
+$destinatario = "contato@deflorealma.com.br";
+
+$nome = $_REQUEST['nome'];
+$email = $_REQUEST['email'];
+$mensagem = $_REQUEST['mensagem'];
+$assunto = "Contato via site deflorealma.com.br";
+
+ // monta o e-mail na variavel $body
+
+$body = "===================================" . "\n";
+$body = $body . "SOLICITAÇÃO DE ORCAMENTO VIA SITE DE FLOR E ALMA" . "\n";
+$body = $body . "===================================" . "\n\n";
+$body = $body . "Nome: " . $nome . "\n";
+$body = $body . "Email: " . $email . "\n";
+$body = $body . "Mensagem: " . $mensagem . "\n\n";
+$body = $body . "===================================" . "\n";
+
+// envia o email
+mail($destinatario, $assunto , $body, "From: $email\r\n");
+
+// redireciona para a página de obrigado
+header("location:buques.html");
+
+
+?>
