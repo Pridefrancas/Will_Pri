a7cec67ccca0d08a68cd7487c91718de17c06f8c
willianfrancas <willianfrancas@gmail.com>
2019-04-18 18:14:00 -0300

    Adição de legenda
    Adição de legenda/caption nas fotos.
    Aplicada somente na pagina buques.html (pagina de testes/implementações)

--- sendmail.php
+++ sendmail.php
@@ -1,92 +1,112 @@
-<?
-if ( class_exists( 'WPForms_Template', false ) ) :
-/**
- * orcamento
- * Template for WPForms.
- */
-class WPForms_Template_orcamento extends WPForms_Template {
+<?php
+	date_default_timezone_set('America/Sao_Paulo');
 
-	/**
-	 * Primary class constructor.
-	 *
-	 * @since 1.0.0
-	 */
-	public function init() {
+	ini_set('display_errors', 1);
+	error_reporting(E_ALL);
+	//Variáveis 
+	$erro = false;
+	$nome = $_POST['nome'];
+	$email = $_POST['email'];
+	$telefone = $_POST['telefone'];
+	$tipo = $_POST['tipo'];
+	$mensagem = $_POST['mensagem'];
+	$data_envio = date('d/m/Y');
+	$hora_envio = date('H:i:s');
 
-		// Template name
-		$this->name = 'orcamento';
-
-		// Template slug
-		$this->slug = 'orcamento';
-
-		// Template description
-		$this->description = '';
-
-		// Template field and settings
-		$this->data = array (
-	'field_id' => 7,
-	'fields' => array (
-		3 => array (
-			'id' => '3',
-			'type' => 'name',
-			'label' => 'Name',
-			'format' => 'simple',
-			'required' => '1',
-			'size' => 'large',
-			'simple_placeholder' => 'Nome',
-			'label_hide' => '1',
-		),
-		4 => array (
-			'id' => '4',
-			'type' => 'number',
-			'required' => '1',
-			'size' => 'large',
-			'placeholder' => 'Telefone p/ Contato',
-		),
-		5 => array (
-			'id' => '5',
-			'type' => 'email',
-			'required' => '1',
-			'size' => 'large',
-			'placeholder' => 'e-mail',
-		),
-		6 => array (
-			'id' => '6',
-			'type' => 'textarea',
-			'size' => 'medium',
-			'placeholder' => 'Conte-nos detalhes do seu evento...',
-		),
-	),
-	'settings' => array (
-		'form_title' => 'orcamento',
-		'submit_text' => 'Submit',
-		'submit_text_processing' => 'Sending...',
-		'honeypot' => '1',
-		'notification_enable' => '1',
-		'notifications' => array (
-			1 => array (
-				'email' => 'contato@deflorealma.com.br',
-				'subject' => 'Contato via site (deflorealma.com.br)',
-				'sender_name' => '{field_id="0"}',
-				'sender_address' => '{admin_email}',
-				'replyto' => '{field_id="1"}',
-				'message' => '{all_fields}',
-			),
-		),
-		'confirmations' => array (
-			1 => array (
-				'type' => 'message',
-				'message' => '<p>Obrigado por entrar em contato!<br />Logo entraremos em contato.</p>',
-				'message_scroll' => '1',
-			),
-		),
-	),
-	'meta' => array (
-		'template' => 'orcamento',
-	),
-);
+	// Verifica se o POST tem algum valor
+	if ( !isset( $_POST ) || empty( $_POST ) ) {
+		$erro = 'Nada foi postado.';
 	}
-}
-new WPForms_Template_orcamento;
-endif;
+	// Cria as variáveis dinamicamente
+	foreach ( $_POST as $chave => $valor ){
+		// Remove todas as tags HTML
+		// Remove os espaços em branco do valor
+		$$chave = trim( strip_tags( $valor ) );
+		// Verifica se tem algum valor nulo
+		if ( empty ( $valor ) ) {
+			$erro = 'Existem campos em branco.';
+		}
+	}
+	// Se existir algum erro, mostra o erro
+	if ( $erro ) {
+		echo $erro;
+	} else {
+		// Se a variável $erro continuar com valor falso
+		// Você pode fazer o que preferir aqui, por exemplo, 
+		// enviar para a base de dados, ou enviar um email
+		// Tanto faz. Vou apenas exibir os dados na tela.
+		//foreach ( $_POST as $chave => $valor ) {
+		//}
+	}
+
+	// Corpo E-mail
+	$arquivo = "<style type='text/css'>
+		body{
+			padding: 2rem 0;
+			background: -webkit-gradient(linear,left top,left bottom,from(rgba(22,22,22,.5)),color-stop(75%,rgba(22,22,22,.6)),to(#535353)),url(https://lh3.googleusercontent.com/pp6HRk0MoINXLfE1GWvIIR4pfri9M3OkvR9mfS0gSmTH_Stxthx3tW1n8ugQrK_IcEpCpn_ACpfdYTTy9xZn77MST1dM1KC0P51wcM8dx0j-p2M3JGOJ0wRZgvNeggwuQG3QQskRzugIPgPo7SmSayGkiBWbY-awPHUpPb9cYpeSOgB0ECCfPfc_o7y0-2bz3oU4cc1CMfDyTBlJNOvziwodPJfzQpGm6Cim2kLoB8EHXCm6hbqkAj5dIfYoS3HjxY1d0q9Qpq1L6942kmz0IpMLL5rjwyzQox6adBzWy4l6w1GcwwMLTyyLeisoLQNwgtC-P3M61qiYcK_4ZWwUuiPTHlRw5z38npy943wrC4AWEwHK-Dpm1E77V2NpFduR22tPFqcajXnugVZnZTG3o0qzu5J6lAfpQ0qQp2Tt1-AVZLPXjRuxu1IS6ifDnkB2dvr9aT0dABSiJbsJM5HrqzKkbtNoXxPMkahYVuDr1FwDfOdoXsy_pGZy4SeK66gciIXgIblpHgEb-BxwBVrw5tBTO0crS4XFkquOUkCsv-v6eJCZSDKF8rr_gJqSf0vGdpBEUYCa2EYipCfgr1bCYnNyORVBe5hAFqo5Bmg=w930-h641);
+    		background-position: center;
+    		background-repeat: no-repeat;
+    		background-attachment: scroll;
+    		background-size: cover;
+    		width: 90%;
+			margin:0px;
+			font-family:Verdane;
+			font-size:12px;
+			color: #666666;
+		}
+		a{
+			color: #666666;
+			text-decoration: none;
+		}
+		a:hover{
+			color: #FF0000;
+			text-decoration: none;
+		}
+		</style>
+		<html style='padding: 2rem 0; background: -webkit-gradient(linear,left top,left bottom,from(rgba(22,22,22,.5)),color-stop(75%,rgba(22,22,22,.6)),to(#535353)),url(https://lh3.googleusercontent.com/pp6HRk0MoINXLfE1GWvIIR4pfri9M3OkvR9mfS0gSmTH_Stxthx3tW1n8ugQrK_IcEpCpn_ACpfdYTTy9xZn77MST1dM1KC0P51wcM8dx0j-p2M3JGOJ0wRZgvNeggwuQG3QQskRzugIPgPo7SmSayGkiBWbY-awPHUpPb9cYpeSOgB0ECCfPfc_o7y0-2bz3oU4cc1CMfDyTBlJNOvziwodPJfzQpGm6Cim2kLoB8EHXCm6hbqkAj5dIfYoS3HjxY1d0q9Qpq1L6942kmz0IpMLL5rjwyzQox6adBzWy4l6w1GcwwMLTyyLeisoLQNwgtC-P3M61qiYcK_4ZWwUuiPTHlRw5z38npy943wrC4AWEwHK-Dpm1E77V2NpFduR22tPFqcajXnugVZnZTG3o0qzu5J6lAfpQ0qQp2Tt1-AVZLPXjRuxu1IS6ifDnkB2dvr9aT0dABSiJbsJM5HrqzKkbtNoXxPMkahYVuDr1FwDfOdoXsy_pGZy4SeK66gciIXgIblpHgEb-BxwBVrw5tBTO0crS4XFkquOUkCsv-v6eJCZSDKF8rr_gJqSf0vGdpBEUYCa2EYipCfgr1bCYnNyORVBe5hAFqo5Bmg=w930-h641);    background-position: center;
+		background-repeat: no-repeat;
+		background-attachment: scroll;
+		background-size: cover; width: 90%;'>
+			<table width='510' border='1' cellpadding='1' cellspacing='1' style='-webkit-border-radius: 16px 16px 16px 16px; border: 5px double #d8e7f3; box-shadow: 4px 4px 4px 4px rgba(216,231,243,1); padding: 8px;'>
+				<tr>
+					<td>
+						<tr>
+							<td width='500'>Nome: <b> $nome </b> </td>
+						</tr>
+						<tr>
+							<td width='320'>E-mail: <b> $email </b> </td>
+						</tr>
+						<tr>
+							<td width='320'>Telefone: <b> $telefone </b> </td>
+						</tr>
+						<tr>
+							<td width='320'>Tipo de evento: <b> $tipo </b> </td>
+						</tr>
+						<tr>
+							<td width='320'>Mensagem: <b> $mensagem </b> </td>
+						</tr>
+					</td>
+				</tr>
+				<tr>
+					<td>Este e-mail foi enviado em <b> $data_envio </b> às <b> $hora_envio </b></td>
+				</tr>
+
+			</table>
+		</html>";
+	//enviar
+	// emails para quem será enviado o formulário
+	$emailenviar = "contato@deflorealma.com.br, aline.teixeiradoliveira@gmail.com";
+	$destino = $emailenviar;
+	$assunto = "Contato pelo Site deflorealma.com.br";
+	 // É necessário indicar que o formato do e-mail é html
+	$headers = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+	$headers .= 'From: Contato <contato@deflorealma.com.br>';
+	//$headers .= "Bcc: $EmailPadrao\r\n";   
+	$enviaremail = mail($destino, $assunto, $arquivo, $headers);
+	if($enviaremail){
+		echo "Mensagem enviada! :) Obrigado por seu contato, em breve lhe responderemos!".'<meta http-equiv="refresh" content="3;URL=index.html" />';
+	}else{
+		echo "Desculpe, a mensagem não pode ser enviada. :´(";
+	}
 ?>
---
+++ sendmail2.php
@@ -0,0 +1,31 @@
+<?php 
+function sanitize_my_email($field) {
+    $field = filter_var($field, FILTER_SANITIZE_EMAIL);
+    if (filter_var($field, FILTER_VALIDATE_EMAIL)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+$nome = $_POST['Nome'];
+$email = $_POST['Email'];
+$fone = $_POST['Fone'];
+$tipo = $_POST['Tipo'];
+$mensagem = $_POST['Mensagem'];
+
+$to_email = 'contato@deflorealma.com.br';
+$subject = 'Contato via site (deflorealma.com.br)';
+//$message = 'This mail is sent using the PHP mail ';
+$message ='Nome: '.$nome."\r\n".'e-Mail: '.$email."\r\n".'Telefone: '.$fone."\r\n".'Tipo de evento: '.$tipo."\r\n".'Comentario: '.$mensagem;
+$headers = 'From: contato@deflorealma.com.br';
+//check if the email address is invalid $secure_check
+$secure_check = sanitize_my_email($to_email);
+if ($secure_check == false) {
+    echo "Desculpe, a mensagem não pode ser enviada. :´(";
+
+} else { //send email 
+    mail($to_email, $subject, $message, $headers);
+    echo "Mensagem enviada. Obrigado por seu contato, em breve entraremos em contato!".'<meta http-equiv="refresh" content="3;URL=index.html" />';
+}
+?>
